bf93dbe2deab302c73711172a403946844896ec4
TKaushik15 <46028294+TKaushik15@users.noreply.github.com>
2020-07-30 11:00:15 +0530

    Update rotateLeft.java

--- rotateLeft.java
+++ rotateLeft.java
@@ -1,4 +1,4 @@
-import java.util.*;
+import java.util.Scanner;
 
 public class left_rotation {
 
@@ -7,24 +7,24 @@ public class left_rotation {
        Scanner sc=new Scanner(System.in);
        int n=sc.nextInt();
        int s=sc.nextInt();
-       int a[]=new int[s];
+       int arr1[]=new int[s];
        int ss[]=new int[n-s];
        for(int i=0;i<s;i++)
        {
-           a[i]=sc.nextInt();
+           arr1[i]=sc.nextInt();
        }   
     for(int j=0;j<n-s;j++)
     {
-        ss[j]=sc.nextInt();
+        arr2[j]=sc.nextInt();
     }
     sc.close();
-    for(int i=0;i<ss.length;i++)
+    for(int i=0;i<arr2.length;i++)
     {
-    System.out.print(ss[i]+" ");
+    System.out.print(arr2[i]+" ");
     }
-    for(int y=0;y<a.length;y++)
+    for(int y=0;y<arr.length;y++)
     {
-        System.out.print(a[y]+" ");
+        System.out.print(arr[y]+" ");
     }
     }
 }
