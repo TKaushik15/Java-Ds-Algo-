8d5f800111ea95742b2dca1f738ebce03b9b7cd4
TKaushik15 <46028294+TKaushik15@users.noreply.github.com>
2020-07-30 10:57:57 +0530

    Update bubbleSort.java

--- bubbleSort.java
+++ bubbleSort.java
@@ -4,6 +4,7 @@ class bubble_sort
 {
     public static void main(String[] args)
   {
+	    //IN BUBBLE SORT, WE COMPARE 2 ADJACENT ELEMENTS AND CHECK WHICH ONE IS GREATER AND SWAP THEM ACCORDINGLY USING A TEMPORARY VARIABLE temp
 	  Scanner sc=new Scanner(System.in);
 	  int n=sc.nextInt();
 	  int a[]=new int[n];
@@ -11,11 +12,12 @@ class bubble_sort
 	  {
 		  a[i]=sc.nextInt();
       }
-      sc.close();
+      
 	  for(int i=0;i<n;i++)
 	  {
 		  for(int j=0;j<n-1;j++)
 		  {
+			  //COMPARING ADJACENT INDEX ELEMENTS
 			  if(a[j]>a[j+1])
 			  {
 				  int temp=a[j];
